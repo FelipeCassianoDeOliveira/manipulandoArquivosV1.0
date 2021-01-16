1b28324f3b29ceded5f71c1f1799fa34122707bc
Felipe Cassiano <Felipe Cassiano@FELIPE_CASSIANO>
2021-01-16 00:05:19 -0300

    Biblioteca itext add e import

--- manipulandoArquivosV1.0/src/manipulandoarquivosv1/pkg0/ManipulandoArquivosV10.java
+++ manipulandoArquivosV1.0/src/manipulandoarquivosv1/pkg0/ManipulandoArquivosV10.java
@@ -9,11 +9,26 @@ package manipulandoarquivosv1.pkg0;
  *
  * @author Felipe Cassiano
  */
+
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//BIBLIOTECA ITEXT
+import com.itextpdf.layout.Document;
+import com.itextpdf.kernel.PdfException;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.kernel.pdf.PdfWriter;
+
+
 public class ManipulandoArquivosV10 {
 
     /**
      * @param args the command line arguments
      */
+    
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
